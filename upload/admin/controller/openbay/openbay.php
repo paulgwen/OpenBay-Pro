7f94dc4450601d2d2b713c93a5c06b45ee8c766e
James Allsup <james@allsup.eu>
2014-11-13 12:17:39 +0000

    Fixed image info issue for additional images in ebay new listing

--- upload/admin/controller/openbay/openbay.php
+++ upload/admin/controller/openbay/openbay.php
@@ -1327,7 +1327,7 @@ class ControllerOpenbayOpenbay extends Controller {
 
 				foreach ($product_images as $product_image) {
 					if ($product_image['image'] && file_exists(DIR_IMAGE . $product_image['image'])) {
-						$img_info = getimagesize(HTTPS_CATALOG . 'image/' . $product_image['image']);
+						$img_info = getimagesize(DIR_IMAGE . $product_image['image']);
 
 						$product_info['product_images'][] = array(
 							'image' => $product_image['image'],
