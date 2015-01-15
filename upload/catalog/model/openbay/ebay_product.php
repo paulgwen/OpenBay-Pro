71f58a2df3ba4355dbb35521c08fb24b669a4bd1
James Allsup <james@allsup.eu>
2015-01-15 23:06:48 +0000

    Added repair category method after product import from eBay to save it being run manually.

--- upload/catalog/model/openbay/ebay_product.php
+++ upload/catalog/model/openbay/ebay_product.php
@@ -194,6 +194,8 @@ class ModelOpenbayEbayProduct extends Model {
 				}
 			}
 
+			$this->repairCategories();
+
 			$this->openbay->ebay->log('Categories done');
 		} else {
 			$this->openbay->ebay->log('Categories set not to be created');
@@ -792,6 +794,30 @@ class ModelOpenbayEbayProduct extends Model {
 			return $this->config->get('config_ssl') . 'image/' . $new_image;
 		} else {
 			return $this->config->get('config_url') . 'image/' . $new_image;
+		}
+	}
+
+	private function repairCategories($parent_id = 0) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category WHERE parent_id = '" . (int)$parent_id . "'");
+
+		foreach ($query->rows as $category) {
+			// Delete the path below the current one
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "category_path` WHERE category_id = '" . (int)$category['category_id'] . "'");
+
+			// Fix for records with no paths
+			$level = 0;
+
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "category_path` WHERE category_id = '" . (int)$parent_id . "' ORDER BY level ASC");
+
+			foreach ($query->rows as $result) {
+				$this->db->query("INSERT INTO `" . DB_PREFIX . "category_path` SET category_id = '" . (int)$category['category_id'] . "', `path_id` = '" . (int)$result['path_id'] . "', level = '" . (int)$level . "'");
+
+				$level++;
+			}
+
+			$this->db->query("REPLACE INTO `" . DB_PREFIX . "category_path` SET category_id = '" . (int)$category['category_id'] . "', `path_id` = '" . (int)$category['category_id'] . "', level = '" . (int)$level . "'");
+
+			$this->repairCategories($category['category_id']);
 		}
 	}
 }
