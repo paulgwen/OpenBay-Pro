de4cf6052688dab32e9c369b1a2392161c2c70b1
James Allsup <james@allsup.eu>
2015-04-01 14:09:30 +0100

    Fix missing language strings for yes/no in category listing auto-map.

--- upload/admin/controller/openbay/ebay_listing.php
+++ upload/admin/controller/openbay/ebay_listing.php
@@ -296,6 +296,8 @@ class ControllerOpenbayEbayListing extends Controller {
 		$this->data['text_listing_30day'] = $this->language->get('text_listing_30day');
 		$this->data['text_listing_gtc'] = $this->language->get('text_listing_gtc');
 		$this->data['text_none'] = $this->language->get('text_none');
+		$this->data['text_yes'] = $this->language->get('text_yes');
+		$this->data['text_no'] = $this->language->get('text_no');
 		$this->data['help_category_popular'] = $this->language->get('help_category_popular');
 
 		$this->data['profiles']['shipping'] = $this->model_openbay_ebay_profile->getAll(0);
