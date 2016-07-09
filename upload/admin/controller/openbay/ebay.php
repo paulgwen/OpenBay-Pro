9a435ccde8d2150bb0e482b16cb660b9af15780d
James Allsup <james@allsup.eu>
2016-07-09 23:52:27 +0100

    Fixed return_policy variable which was spelt and used incorrectly as returns_policy. Although all other fields use the s, this field does not.

--- upload/admin/controller/openbay/ebay.php
+++ upload/admin/controller/openbay/ebay.php
@@ -1911,8 +1911,8 @@ class ControllerOpenbayEbay extends Controller {
                     if (isset($profile_return['data']['returns_accepted'])) {
                         $data['returns_accepted'] = $profile_return['data']['returns_accepted'];
                     }
-                    if (isset($profile_return['data']['returns_policy'])) {
-                        $data['return_policy'] = $profile_return['data']['returns_policy'];
+                    if (isset($profile_return['data']['return_policy'])) {
+                        $data['return_policy'] = $profile_return['data']['return_policy'];
                     }
                     if (isset($profile_return['data']['returns_option'])) {
                         $data['returns_option'] = $profile_return['data']['returns_option'];
@@ -2231,8 +2231,8 @@ class ControllerOpenbayEbay extends Controller {
                     if (isset($profile_return['data']['returns_accepted'])) {
                         $data['returns_accepted'] = $profile_return['data']['returns_accepted'];
                     }
-                    if (isset($profile_return['data']['returns_policy'])) {
-                        $data['return_policy'] = $profile_return['data']['returns_policy'];
+                    if (isset($profile_return['data']['return_policy'])) {
+                        $data['return_policy'] = $profile_return['data']['return_policy'];
                     }
                     if (isset($profile_return['data']['returns_option'])) {
                         $data['returns_option'] = $profile_return['data']['returns_option'];
