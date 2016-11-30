79f3a4470bc9f61281db1d813cfffafc11d33fed
James Allsup <james@allsup.eu>
2016-11-30 03:40:56 +0000

    Added new order notification for admin on new Etsy orders, feature already existed for eBay and Amazon orders.

--- upload/catalog/model/openbay/etsy_order.php
+++ upload/catalog/model/openbay/etsy_order.php
@@ -33,14 +33,22 @@ class ModelOpenbayEtsyOrder extends Model {
 				} else {
 					$order_id = $this->create($order);
 
+					$order_status_id = $this->config->get('etsy_order_status_new');
+
 					// is paid?
 					if ($order->paid == 1) {
+						$order_status_id = $this->config->get('etsy_order_status_paid');
 						$this->updatePaid($order_id, $order->paid);
 
 						// is shipped?
 						if ($order->shipped == 1) {
+							$order_status_id = $this->config->get('etsy_order_status_shipped');
 							$this->updateShipped($order_id, $order->shipped);
 						}
+					}
+
+					if($this->config->get('openbay_amazon_notify_admin') == 1){
+						$this->openbay->newOrderAdminNotify($order_id, $order_status_id);
 					}
 
 					$this->openbay->etsy->log('Created new order: ' . $order_id);
