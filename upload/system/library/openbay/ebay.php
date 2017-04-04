00f883a1e730f982dadd0f295a45c525b5dc527e
James Allsup <james@allsup.eu>
2017-04-04 05:19:53 +0100

    Fix eBay getProductId method. Added correct sort order on the status to always filter by enabled items first in the database result.

--- upload/system/library/openbay/ebay.php
+++ upload/system/library/openbay/ebay.php
@@ -899,7 +899,7 @@ final class Ebay {
 			$status_sql = ' AND `status` = 1';
 		}
 
-		$qry = $this->db->query("SELECT `product_id` FROM `" . DB_PREFIX . "ebay_listing` WHERE `ebay_item_id` = '" . $this->db->escape($ebay_item) . "'" . $status_sql . " ORDER BY `status`, `ebay_listing_id` DESC LIMIT 1");
+		$qry = $this->db->query("SELECT `product_id` FROM `" . DB_PREFIX . "ebay_listing` WHERE `ebay_item_id` = '" . $this->db->escape($ebay_item) . "'" . $status_sql . " ORDER BY `status` DESC, `ebay_listing_id` DESC LIMIT 1");
 
 		if (!$qry->num_rows) {
 			return false;
